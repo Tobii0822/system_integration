a051b342abdc518063e09a8d733f678d5a1cd557
Tobii0822 <panganibankennedy08@gmail.com>
2026-06-10 15:57:42 +0800

    Added

---
+++ app/pages/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="text-align: center; margin-top: 50px;">
+    <h1>LogInPage</h1>
+    <v-card style="max-width: 600px; margin: 50px auto; padding: 20px;">
+      <v-text-field
+      label="Email"
+      type="email"
+      outlined
+      style="max-width: 600px; margin: 20px auto;">
+      </v-text-field>
+
+      <v-text-field
+      label="Password"
+      type="password"
+      outlined
+      style="max-width: 600px; margin: 20px auto; border-radius: 50px;">
+      </v-text-field>
+
+      <v-btn block variant="tonal" color="primary">Log In</v-btn>
+
+    </v-card>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
